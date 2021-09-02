33df66318d538c06376e42800cdb8ee91919c6b8
Kuldeep <kp9017472041@gmail.com>
2021-09-02 19:31:11 +0530

    changes

--- resources/views/layouts/mains.blade.php
+++ resources/views/layouts/mains.blade.php
@@ -15,8 +15,7 @@
     <!--<script src="http://localhost/video-webcam/js/app.js" defer></script>-->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
 
-    <link href="./css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
-    <link href="./css/components.css" rel="stylesheet" type="text/css">
+   
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
@@ -31,7 +30,7 @@
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+
     <!-- Fontawesome CSS -->
     <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
     <!-- Lineawesome CSS -->
@@ -40,55 +39,7 @@
 
 
     <!-- Datatable CSS -->
-    <link rel="stylesheet" href="./assets/css/dataTables.bootstrap4.min.css">
 
-    <link rel="stylesheet" href="./assets/plugins/morris/morris.css">
-    <!-- Select2 CSS -->
-    <link rel="stylesheet" href="./assets/css/select2.min.css">
-
-    <!-- Main CSS -->
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <!-- Datetimepicker CSS -->
-    <link rel="stylesheet" href="./assets/css/bootstrap-datetimepicker.min.css">
-
-
-    <!-- Core JS files -->
-    <script type="text/javascript" src="./js/plugins/loaders/pace.min.js"></script>
-    <script type="text/javascript" src="./js/core/libraries/bootstrap.min.js"></script>
-    <script type="text/javascript" src="./js/plugins/loaders/blockui.min.js"></script>
-    <!-- /core JS files -->
-
-
-    <!-- Theme JS files -->
-
-    <script type="text/javascript" src="./js/plugins/pickers/datepicker.js"></script>
-
-    <!-- Bootstrap Core JS -->
-    <script src="./assets/js/popper.min.js"></script>
-    <script src="./assets/js/bootstrap.min.js"></script>
-
-    <!-- Slimscroll JS -->
-    <script src="./assets/js/jquery.slimscroll.min.js"></script>
-
-    <!-- Chart JS -->
-    <script src="./assets/plugins/morris/morris.min.js"></script>
-    <script src="./assets/plugins/raphael/raphael.min.js"></script>
-    <!-- Select2 JS -->
-    <script src="./assets/js/select2.min.js"></script>
-
-    <!-- Datetimepicker JS -->
-    <script src="./assets/js/moment.min.js"></script>
-    <script src="./assets/js/bootstrap-datetimepicker.min.js"></script>
-
-    <!-- Custom JS -->
-    <script src="./assets/js/app.js"></script>
-
-    <!--<script type="text/javascript" src="/js/pages/dashboard.js"></script>-->
-    <!-- /theme JS files -->
-
-    <!-- Datatable JS -->
-    <script src="./assets/js/jquery.dataTables.min.js"></script>
-    <script src="./assets/js/dataTables.bootstrap4.min.js"></script>
 
                          <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
                          <script src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js"></script>
