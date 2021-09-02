0fba740474a64369dac8fc606fcc229a0bd9c631
Kuldeep <kp9017472041@gmail.com>
2021-09-02 18:26:11 +0530

    new file

---
+++ database/migrations/2014_10_12_000000_create_video_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('video');
+            $table->string('name');
+            $table->string('title');
+            $table->string('duration');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video');
+    }
+}
