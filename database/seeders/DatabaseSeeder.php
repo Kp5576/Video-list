bc8704d3d37bb3130720563b019f8d6d81d3d49f
Kuldeep <kp9017472041@gmail.com>
2021-09-03 12:07:47 +0530

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $faker = Faker::create();
+
+        foreach (range(1,200) as $index) {
+            DB::table('video')->insert([
+                'video' => $faker->video,
+                'name' => $faker->name,
+                'title' => $faker->title,
+                'duration' => $faker->duration,
+              
+            ]);
+        }
     }
 }
