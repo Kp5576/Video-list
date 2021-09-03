86410baf0ec241a794c6533fe92045c678c087e8
Kuldeep <kp9017472041@gmail.com>
2021-09-03 15:12:20 +0530

    chnges

--- resources/views/layouts/mains.blade.php
+++ resources/views/layouts/mains.blade.php
@@ -11,57 +11,87 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Scripts -->
+    <!--<script src="{{ asset('js/app.js') }}" defer></script>-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
 
-  <!-- Scripts -->
-        <!--<script src="./js/app.js" defer></script>-->
-        <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+    <link href="{{ secure_asset('css/icons/icomoon/styles.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ secure_asset('css/components.css') }}" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
 
-        <link href="./css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
-        <link href="./css/components.css" rel="stylesheet" type="text/css">
-
-
-        <!-- Global stylesheets -->
-        <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-
-
-
-        <!-- /global stylesheets -->
-
-        <!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
-        <!-- Fontawesome CSS -->
-        <link rel="stylesheet" href="./assets/css/font-awesome.min.css">
-        <!-- Lineawesome CSS -->
-        <link rel="stylesheet" href="./assets/css/line-awesome.min.css">
-        <!-- Chart CSS -->
-
-
-        <!-- Datatable CSS -->
-        <link rel="stylesheet" href="./assets/css/dataTables.bootstrap4.min.css">
-
-        <link rel="stylesheet" href="./assets/plugins/morris/morris.css">
-        <!-- Select2 CSS -->
-        <link rel="stylesheet" href="./assets/css/select2.min.css">
-
-        <!-- Main CSS -->
-        <link rel="stylesheet" href="./assets/css/style.css">
-        <!-- Datetimepicker CSS -->
-        <link rel="stylesheet" href="./assets/css/bootstrap-datetimepicker.min.css">
+    <!-- Global stylesheets -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
 
 
 
+    <!-- /global stylesheets -->
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
-        <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
-        <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap.min.css') }}">
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/font-awesome.min.css') }}">
+    <!-- Lineawesome CSS -->
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/line-awesome.min.css') }}">
+    <!-- Chart CSS -->
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Datatable CSS -->
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/dataTables.bootstrap4.min.css') }}">
+
+    <link rel="stylesheet" href="{{ secure_asset('assets/plugins/morris/morris.css') }}">
+    <!-- Select2 CSS -->
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/select2.min.css') }}">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
+    <!-- Datetimepicker CSS -->
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap-datetimepicker.min.css') }}">
+
+
+    <!-- Core JS files -->
+    <script type="text/javascript" src="{{ secure_asset('js/plugins/loaders/pace.min.js') }}"></script>
+    <script type="text/javascript" src="{{ secure_asset('js/core/libraries/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ secure_asset('js/plugins/loaders/blockui.min.js') }}"></script>
+    <!-- /core JS files -->
+
+
+    <!-- Theme JS files -->
+
+    <script type="text/javascript" src="{{ secure_asset('js/plugins/pickers/datepicker.js') }}"></script>
+
+    <!-- Bootstrap Core JS -->
+    <script src="{{ secure_asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/js/bootstrap.min.js') }}"></script>
+
+    <!-- Slimscroll JS -->
+    <script src="{{ secure_asset('assets/js/jquery.slimscroll.min.js') }}"></script>
+
+    <!-- Chart JS -->
+    <script src="{{ secure_asset('assets/plugins/morris/morris.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/plugins/raphael/raphael.min.js') }}"></script>
+    <!-- Select2 JS -->
+    <script src="{{ secure_asset('assets/js/select2.min.js') }}"></script>
+
+    <!-- Datetimepicker JS -->
+    <script src="{{ secure_asset('assets/js/moment.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/js/bootstrap-datetimepicker.min.js') }}"></script>
+
+    <!-- Custom JS -->
+    <script src="{{ secure_asset('assets/js/app.js') }}"></script>
+
+    <!--<script type="text/javascript" src="{{ asset('js/pages/dashboard.js') }}"></script>-->
+    <!-- /theme JS files -->
+
+    <!-- Datatable JS -->
+    <script src="{{ secure_asset('assets/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/js/dataTables.bootstrap4.min.js') }}"></script>
+
+
+
 
 
 </head>
